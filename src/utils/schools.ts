cbea23019531e890b9e6f779e3440d5a27efa432
aikaioutway-ai <aikai.outway@gmail.com>
2026-06-11 21:31:21 +0600

    Add short abbreviations to SCHOOLS

--- src/utils/schools.ts
+++ src/utils/schools.ts
@@ -1,19 +1,19 @@
 import { School, SchoolCode } from '../types';
 
 export const SCHOOLS: School[] = [
-  { code: 'KINGS',   name: 'Kings',          zones: 3 },
-  { code: 'LIGHT',   name: 'Light Academy',  zones: 3 },
-  { code: 'BILIM',   name: 'Bilim KG',       zones: 3 },
-  { code: 'AES',     name: 'AES',            zones: 3 },
-  { code: 'KAS',     name: 'KAS',            zones: 3 },
-  { code: 'EPSILON', name: 'Epsilon',         zones: 3 },
-  { code: 'GENIUS',  name: 'Genius',          zones: 3 },
-  { code: 'GENIUS4', name: 'Genius 4',        zones: 3 },
-  { code: 'NOVA',    name: 'Nova',            zones: 3 },
-  { code: 'INDIGO',  name: 'Indigo',          zones: 3 },
-  { code: 'ERUDIT',  name: 'Erudit',          zones: 2 },
-  { code: 'TENSAY',  name: 'Tensay',          zones: 2 },
-  { code: 'EDISON',  name: 'Edison',          zones: 2 },
+  { code: 'KINGS',   name: 'Kings',         short: 'KNG',    zones: 3 },
+  { code: 'LIGHT',   name: 'Light Academy', short: 'LA',     zones: 3 },
+  { code: 'BILIM',   name: 'Bilim KG',      short: 'BKG',    zones: 3 },
+  { code: 'AES',     name: 'AES',           short: 'AES',    zones: 3 },
+  { code: 'KAS',     name: 'KAS',           short: 'KAS',    zones: 3 },
+  { code: 'EPSILON', name: 'Epsilon',        short: 'EPS',    zones: 3 },
+  { code: 'GENIUS',  name: 'Genius',         short: 'GEN #2', zones: 3 },
+  { code: 'GENIUS4', name: 'Genius 4',       short: 'GEN #4', zones: 3 },
+  { code: 'NOVA',    name: 'Nova',           short: 'NOVA',   zones: 3 },
+  { code: 'INDIGO',  name: 'Indigo',         short: 'ING',    zones: 3 },
+  { code: 'ERUDIT',  name: 'Erudit',         short: 'ERU',    zones: 2 },
+  { code: 'TENSAY',  name: 'Tensay',         short: 'TIS',    zones: 2 },
+  { code: 'EDISON',  name: 'Edison',         short: 'EDI',    zones: 2 },
 ];
 
 export function schoolByCode(code: SchoolCode): School | undefined {
